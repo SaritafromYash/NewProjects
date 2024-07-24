fcc209b0a7f757bf99303c3cb491878acfde546e
Sarita  Lad <sarita.lad@Yash.com>
2024-07-24 17:50:38 +0530

    routing demo 16

---
+++ RoutingDemo/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { DataComponent } from './data/data.component';
+import { FirstComponent } from './first/first.component';
+
+export const routes: Routes = [
+{path:"data",component:DataComponent},
+{path:"first",component:FirstComponent}
+
+
+];
